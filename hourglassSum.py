ceb23775aaefac91375313e7dea28b4f58792b7f
Diego Botero <diego.botero@correounivalle.edu.co>
2018-08-03 22:25:40 -0500

    stash applied

--- hourglassSum.py
+++ hourglassSum.py
@@ -7,8 +7,15 @@ import sys
 # From:
 # https://www.hackerrank.com/challenges/2d-array/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
 
+
+# Recibe una matriz cuadrada 6x6, la cual contiene relojes de arena.
+# Devuelve la mayor entre las sumas de los elementos de cada reloj de arena.
 def hourglassSum(arr):
 
+for i in range(6):
+	for j in range(6):
+		
+		
 
 
 
